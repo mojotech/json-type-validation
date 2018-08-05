8c50427b24f6c4ef8f79c3ee5a250288ddc3c9e8
Elias Mulhall <elias@mojotech.com>
2018-08-05 00:27:38 -0400

    Remove type annotations from combinators
    The tslint 'variable-name' rule checks that you don't use typescript
    keywords such as `string`, `number`, and `boolean` as variables. In this
    case that's exactly what we want to do.

--- src/combinators.ts
+++ src/combinators.ts
@@ -1,75 +1,54 @@
-import {Decoder, DecoderObject} from './decoder';
+import {Decoder} from './decoder';
+
+/* tslint:disable:variable-name */
 
 /** See `Decoder.string` */
-export function string(): Decoder<string> {
-  return Decoder.string();
-}
+export const string = Decoder.string;
 
 /** See `Decoder.number` */
-export function number(): Decoder<number> {
-  return Decoder.number();
-}
+export const number = Decoder.number;
 
 /** See `Decoder.boolean` */
-export function boolean(): Decoder<boolean> {
-  return Decoder.boolean();
-}
+export const boolean = Decoder.boolean;
 
 /** See `Decoder.anyJson` */
-export const anyJson: () => Decoder<any> = Decoder.anyJson;
+export const anyJson = Decoder.anyJson;
 
 /** See `Decoder.constant` */
-export function constant(value: true): Decoder<true>;
-export function constant(value: false): Decoder<false>;
-export function constant<A>(value: A): Decoder<A>;
-export function constant(value: any): Decoder<any> {
-  return Decoder.constant(value);
-}
+export const constant = Decoder.constant;
 
 /** See `Decoder.object` */
-export function object<A>(decoders: DecoderObject<A>): Decoder<A> {
-  return Decoder.object(decoders);
-}
+export const object = Decoder.object;
 
 /** See `Decoder.array` */
-export const array: <A>(decoder: Decoder<A>) => Decoder<A[]> = Decoder.array;
+export const array = Decoder.array;
 
 /** See `Decoder.dict` */
-export const dict: <A>(decoder: Decoder<A>) => Decoder<{[name: string]: A}> = Decoder.dict;
+export const dict = Decoder.dict;
 
 /** See `Decoder.optional` */
-export const optional: <A>(decoder: Decoder<A>) => Decoder<A | undefined> = Decoder.optional;
+export const optional = Decoder.optional;
 
 /** See `Decoder.oneOf` */
-export const oneOf: <A>(...decoders: Decoder<A>[]) => Decoder<A> = Decoder.oneOf;
+export const oneOf = Decoder.oneOf;
 
 /** See `Decoder.union` */
-export function union <A, B>(ad: Decoder<A>, bd: Decoder<B>): Decoder<A | B>; // prettier-ignore
-export function union <A, B, C>(ad: Decoder<A>, bd: Decoder<B>, cd: Decoder<C>): Decoder<A | B | C>; // prettier-ignore
-export function union <A, B, C, D>(ad: Decoder<A>, bd: Decoder<B>, cd: Decoder<C>, dd: Decoder<D>): Decoder<A | B | C | D>; // prettier-ignore
-export function union <A, B, C, D, E>(ad: Decoder<A>, bd: Decoder<B>, cd: Decoder<C>, dd: Decoder<D>, ed: Decoder<E>): Decoder<A | B | C | D | E>; // prettier-ignore
-export function union <A, B, C, D, E, F>(ad: Decoder<A>, bd: Decoder<B>, cd: Decoder<C>, dd: Decoder<D>, ed: Decoder<E>, fd: Decoder<F>): Decoder<A | B | C | D | E | F>; // prettier-ignore
-export function union <A, B, C, D, E, F, G>(ad: Decoder<A>, bd: Decoder<B>, cd: Decoder<C>, dd: Decoder<D>, ed: Decoder<E>, fd: Decoder<F>, gd: Decoder<G>): Decoder<A | B | C | D | E | F | G>; // prettier-ignore
-export function union <A, B, C, D, E, F, G, H>(ad: Decoder<A>, bd: Decoder<B>, cd: Decoder<C>, dd: Decoder<D>, ed: Decoder<E>, fd: Decoder<F>, gd: Decoder<G>, hd: Decoder<H>): Decoder<A | B | C | D | E | F | G | H>; // prettier-ignore
-export function union(ad: Decoder<any>, bd: Decoder<any>, ...ds: Decoder<any>[]): Decoder<any> {
-  return Decoder.oneOf(ad, bd, ...ds);
-}
+export const union = Decoder.union;
 
+/** See `Decoder.intersection` */
 export const intersection = Decoder.intersection;
 
 /** See `Decoder.withDefault` */
-export const withDefault: <A>(defaultValue: A, decoder: Decoder<A>) => Decoder<A> =
-  Decoder.withDefault;
+export const withDefault = Decoder.withDefault;
 
 /** See `Decoder.valueAt` */
-export const valueAt: <A>(paths: (string | number)[], decoder: Decoder<A>) => Decoder<A> =
-  Decoder.valueAt;
+export const valueAt = Decoder.valueAt;
 
 /** See `Decoder.succeed` */
-export const succeed: <A>(fixedValue: A) => Decoder<A> = Decoder.succeed;
+export const succeed = Decoder.succeed;
 
 /** See `Decoder.fail` */
-export const fail: <A>(errorMessage: string) => Decoder<A> = Decoder.fail;
+export const fail = Decoder.fail;
 
 /** See `Decoder.lazy` */
-export const lazy: <A>(mkDecoder: () => Decoder<A>) => Decoder<A> = Decoder.lazy;
+export const lazy = Decoder.lazy;
